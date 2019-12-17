008abe3fdf8f830482759a2141e66935c80bada3
cyn <cmdominguez@gmail.com>
2019-12-16 16:31:55 -0800

    Edit task class and add entry model

--- tasks/models.py
+++ tasks/models.py
@@ -4,9 +4,25 @@ import datetime
 class Task(models.Model):
     '''A task the user intends to complete'''
     text = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        '''Return a string representation of the model.'''
-        return self.text
+    date_completed = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    '''Return a string representation of the model.'''
+    return self.text
+
+class Entry(models.Model):
+    '''The specific parts of a task'''
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    '''Handles plural references of entry'''
+    plural_name = 'entries'
+
+def __str__(self):
+    '''Return a string representation of the model.'''
+    return f"{self.text[:50]}..."
 
