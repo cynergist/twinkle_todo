54eeffb3a0600c9aa4f2805cfe2955249e01fc01
cyn <cmdominguez@gmail.com>
2019-12-17 23:07:37 -0800

    Add tasks page

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,12 @@ app_name = 'tasks'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # About
+    path('about/', views.about, name='about'),
+    # Contact
+    path('contact/', views.contact, name='contact'),
+    # Page that shows all tasks
+    path('tasks/', views.tasks, name='tasks'),
+    # Detail page for a single task
+    path('tasks/<int:task_id>/', views.tasks, name='task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Task
 
 def index(request):
-    '''The hompe page for Twinkle Todo'''
+    '''The home page for Twinkle Todo'''
     return render(request, 'tasks/index.html')
+
+def tasks(request):
+    '''Show all tasks'''
+    tasks = Task.objects.order_by('date_added')
+    context = {'tasks': tasks}
+    return render(request, 'tasks/tasks.html', context)
+
+def about(request):
+    '''The about page for Twinkle Todo'''
+    return render(request, 'tasks/about.html')
+
+def contact(request):
+    '''The contact page for Twinkle Todo'''
+    return render(request, 'tasks/contact.html')
+
+
+
+
