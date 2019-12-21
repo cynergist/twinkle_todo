aafcdc37b2bdbb0a5692e737c913bd571446870f
cyn <cmdominguez@gmail.com>
2019-12-20 20:03:15 -0800

    Add task model tests for modification of content

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ import datetime
 class Task(models.Model):
     '''A task the user intends to complete'''
     text = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField(auto_now=True)
     completed = models.BooleanField(blank=True, null=True) # null=True, default=True
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    '''Tests ability to add content to 'text' and 'description' fields'''
+
+    @classmethod
+    def setUpTestData(cls):
+        Task.objects.create(text='first task')
+        Task.objects.create(description='default description here')
+
+    def test_text_content(self):
+        task = Task.objects.get(id=1)
+        expected_object_name = f'{task.text}'
+        self.assertEquals(expected_object_name, 'first task')
+
+    def test_description_content(self):
+        task = Task.objects.get(id=2)
+        expected_object_name = f'{task.description}'
+        self.assertEquals(expected_object_name, 'default description here')
